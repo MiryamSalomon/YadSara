bb57b92c5e1161d2ebaf58ae2e6bbfc6a97bb4b5
MiryamSalomon <m.design7499@gmail.com>
2024-11-30 19:38:55 +0200

    new project

---
+++ YadSara.Core/Entities/Borrow.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace YadSara.Core.Entities
+{
+    // שואל
+    public class Borrow
+    {
+
+        [Key]
+        public string borrowId { get; set; }
+        public string borrowName { get; set;}
+        public string phone { get; set; }
+        public string address { get; set;}
+        public int cityId { get; set;}
+
+        public Borrow(string borrowId, string borrowName, string phone, string address, int cityId)
+        {
+            this.borrowId = borrowId;
+            this.borrowName = borrowName;
+            this.phone = phone;
+            this.address = address;
+            this.cityId = cityId;
+
+        }
+
+
+    }
+}
